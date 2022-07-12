490a9e9259ed02c39aa7fa053826deabd2d53533
Marcos Valderrabano <ing.marco.valu@gmail.com>
2022-07-11 17:56:24 -0700

    Completed lab

--- looping.rb
+++ looping.rb
@@ -1,5 +1,13 @@
+require "pry"
+
 def happy_new_year
   # your code here
+  counter = 10
+  until counter == 0
+    puts "#{counter}"
+    counter -= 1
+  end
+  puts "Happy New Year!"
 end
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
@@ -17,8 +25,27 @@ end
 
 def fizzbuzz_printer
   # your code here
+  counter = 1
+  until counter == 101
+    if fizzbuzz(counter) == "FizzBuzz"
+      puts "FizzBuzz"
+    elsif fizzbuzz(counter) == "Buzz"
+      puts "Buzz"
+    elsif fizzbuzz(counter) == "Fizz"
+      puts "Fizz"
+    else
+      puts "#{counter}"
+    end
+    counter += 1
+  end
 end
 
 def reverse_string(str)
   # your code here
+  reversed = ""
+  for i in 1..str.length
+    reversed += str[str.length - i]
+  end
+  # binding.pry
+  reversed
 end
